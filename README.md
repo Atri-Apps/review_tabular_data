a4f4f38b75aee7a87b9061d229a5ee95041d9d76
darshitac11 <darshitac11@gmail.com>
2022-09-09 04:54:24 -0400

    frontend only

---
+++ README.md
@@ -0,0 +1,37 @@
+# Atri-Apps/review_tabular_data
+
+This app displays tabular data and allows app users to leave comments 
+
+## Demo
+
+
+## How to use this app?
+
+### Step 1: Setup
+
+```shell
+git clone https://github.com/Atri-Apps/review_tabular_data.git
+
+cd portfolio-template
+
+pipenv install
+```
+
+### Step 2: Start the editor
+
+```shell
+pipenv shell
+
+atri start
+```
+
+## Resources
+📚 Read the [Docs](https://docs.atrilabs.com/)
+
+🧭 Follow Atri Labs on [LinkedIn](https://www.linkedin.com/company/atri-labs)
+
+💬 Join our [Slack community](https://join.slack.com/t/atricommunity/shared_invite/zt-1e756m1at-bZBxngvw7KWWO0riI4pc0w)
+
+🎥 Watch along on [YouTube](https://www.youtube.com/channel/UC1uR2Q5x_8olWS_Y4PdK1Bw)
+
+⭐️ Star [Atri framework](https://github.com/Atri-Labs/atrilabs-engine) if you find it helpful! 😎
---
+++ controllers/routes/main.py
@@ -0,0 +1,71 @@
+from .atri import Atri
+from fastapi import Request, Response
+from atri_utils import *
+import pandas as pd
+
+def init_state(at: Atri):
+    """
+    This function is called everytime "Publish" button is hit in the editor.
+    The argument "at" is a dictionary that has initial values set from visual editor.
+    Changing values in this dictionary will modify the intial state of the app.
+    """
+    df = pd.read_csv('most_popular_books.csv')
+    # df = pd.DataFrame(
+    #     {'id': [1, 2], 'col2': [0.5, 0.75]}, index=['a', 'b'])
+    at.Table3.custom.cols = [
+        {'field':'id', 'headerName':'id'},
+        {'field':'Name', 'headerName':'Name', 'width':400},
+        {'field':'Author', 'headerName':'Author', 'width':200},
+        {'field':'Rating', 'headerName':'Rating'},
+        {'field':'Reviews', 'headerName':'Reviews'},
+        {'field':'Price', 'headerName':'Price'},
+        {'field':'Year', 'headerName':'Year'},
+        {'field':'Genre', 'headerName':'Genre'},
+    ]
+    at.Table3.custom.rows = df.to_dict(orient='records')
+
+    #comment 1
+    at.Table6.custom.cols = [
+        {'field':'id', 'headerName':'id'},
+        {'field':'Name', 'headerName':'Name'},
+        {'field':'Author', 'headerName':'Author'},
+        {'field':'Rating', 'headerName':'Rating'},
+        {'field':'Reviews', 'headerName':'Reviews'},
+        {'field':'Price', 'headerName':'Price'},
+        {'field':'Year', 'headerName':'Year'},
+        {'field':'Genre', 'headerName':'Genre'},
+    ]
+    at.Table6.custom.rows = df.head(1).to_dict(orient='records')
+
+    #comment 2
+    at.Table7.custom.cols = [
+        {'field':'id', 'headerName':'id'},
+        {'field':'Name', 'headerName':'Name'},
+        {'field':'Author', 'headerName':'Author'},
+        {'field':'Rating', 'headerName':'Rating'},
+        {'field':'Reviews', 'headerName':'Reviews'},
+        {'field':'Price', 'headerName':'Price'},
+        {'field':'Year', 'headerName':'Year'},
+        {'field':'Genre', 'headerName':'Genre'},
+    ]
+    at.Table7.custom.rows = df.tail(2).to_dict(orient='records')
+
+    pass
+
+def handle_page_request(at: Atri, req: Request, res: Response, query: str):
+    """
+    This function is called whenever a user loads this route in the browser.
+    """
+    pass
+
+def handle_event(at: Atri, req: Request, res: Response):
+    """
+    This function is called whenever an event is received. An event occurs when user
+    performs some action such as click button.
+    """
+    pass
+
+def write_table(df, cols):
+    at.Table.custom.rows = df.to_dict(orient='records')
+
+    
